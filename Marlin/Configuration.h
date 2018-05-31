13f0b540a27714135288d01a6dd761d7ce90a0b3
Jack Wilsdon <jack.wilsdon@gmail.com>
2018-05-31 22:09:12 +0100

    Speed up nozzle park

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1294,8 +1294,8 @@
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z }
   #define NOZZLE_PARK_POINT { 0, 0, 250 }
-  #define NOZZLE_PARK_XY_FEEDRATE 100   // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
-  #define NOZZLE_PARK_Z_FEEDRATE 5      // Z axis feedrate in mm/s (not used for delta printers)
+  #define NOZZLE_PARK_XY_FEEDRATE 100 // X and Y axes feedrate in mm/s (also used for delta printers Z axis)
+  #define NOZZLE_PARK_Z_FEEDRATE 100  // Z axis feedrate in mm/s (not used for delta printers)
 #endif
 
 /**
